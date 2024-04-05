a58ef716ea4d79220d782c137ed2440440db9207
ζั͡幻ั͡月ั͡✾ <64715639+Tira-tw@users.noreply.github.com>
2024-04-05 19:50:30 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+<h1>shorten link menu</h1>
+
+<nav>
+	<a href="https://tw.url-ross.workers.dev/">繁體中文</a>
+	<a href="https://cn.url-ross.workers.dev/">简体中文</a>
+	<a href="https://en.url-ross.workers.dev/">English</a>
+	<a href="https://kor.url-ross.workers.dev/">한국어</a>
+	<a href="https://idn.url-ross.workers.dev/">Bahasa Indonesia</a>
+	<div class="animation start-home"></div>
+</nav>
+
+<p>
+  By <span>Tira-tw</span>
+</p>
